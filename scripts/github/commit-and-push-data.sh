660b069b28c553efc32ea3c4fbe9b488994156f8
fraz3alpha <andrewtaylor2@gmail.com>
2019-11-01 21:31:51 +0000

    Only push if variable is available

--- scripts/github/commit-and-push-data.sh
+++ scripts/github/commit-and-push-data.sh
@@ -9,5 +9,7 @@ git status
 git commit --message "Travis build: $TRAVIS_BUILD_NUMBER\n\n  - Triggered by ${TRAVIS_EVENT_TYPE}"
 git log -1
 
-git remote add origin-data https://${GITHUB_TOKEN_RUNNING_CHALLENGES_DATA}@github.com/fraz3alpha/running-challenges-data.git
-git push --quiet --set-upstream origin-data test-data-branch
+if [ "${GITHUB_TOKEN_RUNNING_CHALLENGES_DATA}" != "" ]; then 
+    git remote add origin-data https://${GITHUB_TOKEN_RUNNING_CHALLENGES_DATA}@github.com/fraz3alpha/running-challenges-data.git
+    git push --quiet --set-upstream origin-data test-data-branch
+fi
