0fbef3be5403e060d4053d10e2a9e99397876014
fraz3alpha <andrewtaylor2@gmail.com>
2019-11-01 00:08:13 +0000

    Update to git commit message

--- scripts/github/commit-and-push-data.sh
+++ scripts/github/commit-and-push-data.sh
@@ -4,5 +4,5 @@ git config --global user.email "travis-data-auto-updater"
 git config --global user.name "travis-data-auto-updater"
 git add data/
 git status
-git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
+git commit --message "Travis build: $TRAVIS_BUILD_NUMBER\n\n  - Triggered by ${TRAVIS_EVENT_TYPE}"
 git log -1
