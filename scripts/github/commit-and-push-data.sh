6a65600c995e03481cca44e41c5fe70c77da7c83
fraz3alpha <andrewtaylor2@gmail.com>
2019-11-01 21:51:00 +0000

    Only do git if master

--- scripts/github/commit-and-push-data.sh
+++ scripts/github/commit-and-push-data.sh
@@ -9,7 +9,17 @@ git status
 git commit --message "Travis build: $TRAVIS_BUILD_NUMBER\n\n  - Triggered by ${TRAVIS_EVENT_TYPE}"
 git log -1
 
-if [ "${GITHUB_TOKEN_RUNNING_CHALLENGES_DATA}" != "" ]; then 
-    git remote add origin-data https://${GITHUB_TOKEN_RUNNING_CHALLENGES_DATA}@github.com/fraz3alpha/running-challenges-data.git
-    git push --quiet --set-upstream origin-data test-data-branch
+if ["${TRAVIS_BRANCH}" == "master"]; then
+
+    if [ "${GITHUB_TOKEN_RUNNING_CHALLENGES_DATA}" != "" ]; then
+
+        git checkout -b master
+
+        git remote add origin-data https://${GITHUB_TOKEN_RUNNING_CHALLENGES_DATA}@github.com/fraz3alpha/running-challenges-data.git
+        git push --quiet --set-upstream origin-data master
+    else
+        echo "Skipping push as GitHub token not available"
+    fi
+else
+    echo "Skipping git commands as we aren't on master"
 fi
