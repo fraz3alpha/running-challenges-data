c7d92ba46bd008a5e0935f2c2d1b9cb794979abc
fraz3alpha <andrewtaylor2@gmail.com>
2019-11-01 21:12:59 +0000

    Add command to push to repo

--- scripts/github/commit-and-push-data.sh
+++ scripts/github/commit-and-push-data.sh
@@ -6,3 +6,6 @@ git add data/
 git status
 git commit --message "Travis build: $TRAVIS_BUILD_NUMBER\n\n  - Triggered by ${TRAVIS_EVENT_TYPE}"
 git log -1
+
+git remote add origin-data https://${GITHUB_TOKEN_RUNNING_CHALLENGES_DATA}@github.com/fraz3alpha/running-challenges-data.git
+git push --quiet --set-upstream origin-data test-data-branch
