ad81ea18c5f4460f0816f9be3bd90acc2ea0aa4b
fraz3alpha <andrewtaylor2@gmail.com>
2019-11-01 21:16:34 +0000

    push data to test branch

--- scripts/github/commit-and-push-data.sh
+++ scripts/github/commit-and-push-data.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-git config --global user.email "travis-data-auto-updater"
-git config --global user.name "travis-data-auto-updater"
+git checkout -b test-data-branch
+
+git config user.email "travis-data-auto-updater"
+git config user.name "travis-data-auto-updater"
 git add data/
 git status
 git commit --message "Travis build: $TRAVIS_BUILD_NUMBER\n\n  - Triggered by ${TRAVIS_EVENT_TYPE}"
