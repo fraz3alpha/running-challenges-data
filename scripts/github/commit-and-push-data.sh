63901359d28a97a8dd6912164e7b4d20b7435770
fraz3alpha <andrewtaylor2@gmail.com>
2019-11-02 17:38:52 +0000

    Try and fix newlines in git commits

--- scripts/github/commit-and-push-data.sh
+++ scripts/github/commit-and-push-data.sh
@@ -8,7 +8,7 @@ git config user.email "travis-data-auto-updater"
 git config user.name "travis-data-auto-updater"
 git add data/
 git status
-git commit --message "Travis build: $TRAVIS_BUILD_NUMBER\n\n  - Triggered by ${TRAVIS_EVENT_TYPE}"
+git commit --message "Travis build: $TRAVIS_BUILD_NUMBER\\n\\n  - Triggered by ${TRAVIS_EVENT_TYPE}"
 git log -1
 
 if [ "${TRAVIS_BRANCH}" == "master" ]; then
