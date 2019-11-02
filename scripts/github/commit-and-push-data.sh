c32bd8037d644d697e36902161226ca3f4f98669
fraz3alpha <andrewtaylor2@gmail.com>
2019-11-02 17:32:46 +0000

    Move switching to master higher up

--- scripts/github/commit-and-push-data.sh
+++ scripts/github/commit-and-push-data.sh
@@ -1,6 +1,8 @@
 #!/bin/bash -e
 
 git branch
+# Make sure we are on the right branch
+git checkout master
 
 git config user.email "travis-data-auto-updater"
 git config user.name "travis-data-auto-updater"
@@ -12,9 +14,6 @@ git log -1
 if [ "${TRAVIS_BRANCH}" == "master" ]; then
 
     if [ "${GITHUB_TOKEN_RUNNING_CHALLENGES_DATA}" != "" ]; then
-
-        # Make sure we are on the right branch
-        git checkout master
 
         git remote add origin-data https://${GITHUB_TOKEN_RUNNING_CHALLENGES_DATA}@github.com/fraz3alpha/running-challenges-data.git
         echo "Pushing commit to upstream"
